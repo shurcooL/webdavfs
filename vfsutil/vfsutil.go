6e829ce6f6e49222c66135ca0de4f681f1a67040
Dmitri Shuralyov <shurcooL@gmail.com>
2017-05-12 19:00:30 -0400

    vfsutil: Add Context as first parameter.
    This is a breaking API change. To update and preserve previous behavior,
    simply pass context.Background() or context.TODO() as first parameter.
    
    This makes it possible to propagate a Context.
    
    Related to https://github.com/golang/net/commit/4bb47a1098b37d69980d96237e2ae4ff56bb5a95.
    
    Follows 8930cd4f86f5ff8529457c7e81b3c061ac4ccc0f.

--- vfsutil/vfsutil.go
+++ vfsutil/vfsutil.go
@@ -15,22 +15,22 @@ import (
 // it if it already exists. If successful, methods on the returned
 // File can be used for I/O; the associated file descriptor has mode O_RDWR.
 // If there is an error, it will be of type *os.PathError.
-func Create(fs webdav.FileSystem, name string) (webdav.File, error) {
-	return fs.OpenFile(context.Background(), name, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+func Create(ctx context.Context, fs webdav.FileSystem, name string) (webdav.File, error) {
+	return fs.OpenFile(ctx, name, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
 }
 
 // Open opens the named file for reading.  If successful, methods on
 // the returned file can be used for reading; the associated file
 // descriptor has mode O_RDONLY.
 // If there is an error, it will be of type *os.PathError.
-func Open(fs webdav.FileSystem, name string) (http.File, error) {
-	return fs.OpenFile(context.Background(), name, os.O_RDONLY, 0)
+func Open(ctx context.Context, fs webdav.FileSystem, name string) (http.File, error) {
+	return fs.OpenFile(ctx, name, os.O_RDONLY, 0)
 }
 
 // ReadDir reads the contents of the directory associated with file and
 // returns a slice of FileInfo values in directory order.
-func ReadDir(fs webdav.FileSystem, name string) ([]os.FileInfo, error) {
-	f, err := fs.OpenFile(context.Background(), name, os.O_RDONLY, 0)
+func ReadDir(ctx context.Context, fs webdav.FileSystem, name string) ([]os.FileInfo, error) {
+	f, err := fs.OpenFile(ctx, name, os.O_RDONLY, 0)
 	if err != nil {
 		return nil, err
 	}
@@ -41,8 +41,8 @@ func ReadDir(fs webdav.FileSystem, name string) ([]os.FileInfo, error) {
 // WriteFile writes data to a file named by name.
 // If the file does not exist, WriteFile creates it with permissions perm;
 // otherwise WriteFile truncates it before writing.
-func WriteFile(fs webdav.FileSystem, name string, data []byte, perm os.FileMode) error {
-	f, err := fs.OpenFile(context.Background(), name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
+func WriteFile(ctx context.Context, fs webdav.FileSystem, name string, data []byte, perm os.FileMode) error {
+	f, err := fs.OpenFile(ctx, name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
 	if err != nil {
 		return err
 	}
@@ -60,9 +60,9 @@ func WriteFile(fs webdav.FileSystem, name string, data []byte, perm os.FileMode)
 // and returns nil, or else returns an error. The permission bits perm are used
 // for all directories that MkdirAll creates. If path is already a directory,
 // MkdirAll does nothing and returns nil.
-func MkdirAll(fs webdav.FileSystem, path string, perm os.FileMode) error {
+func MkdirAll(ctx context.Context, fs webdav.FileSystem, path string, perm os.FileMode) error {
 	// Fast path: if we can tell whether path is a directory or file, stop with success or error.
-	dir, err := fs.Stat(context.Background(), path)
+	dir, err := fs.Stat(ctx, path)
 	if err == nil {
 		if dir.IsDir() {
 			return nil
@@ -83,17 +83,17 @@ func MkdirAll(fs webdav.FileSystem, path string, perm os.FileMode) error {
 
 	if j > 1 {
 		// Create parent.
-		err = MkdirAll(fs, path[0:j-1], perm)
+		err = MkdirAll(ctx, fs, path[0:j-1], perm)
 		if err != nil {
 			return err
 		}
 	}
 
 	// Parent now exists; invoke Mkdir and use its result.
-	err = fs.Mkdir(context.Background(), path, perm)
+	err = fs.Mkdir(ctx, path, perm)
 	if err != nil {
 		// Handle arguments like "foo/." by double-checking that directory doesn't exist.
-		dir, err1 := fs.Stat(context.Background(), path)
+		dir, err1 := fs.Stat(ctx, path)
 		if err1 == nil && dir.IsDir() {
 			return nil
 		}
